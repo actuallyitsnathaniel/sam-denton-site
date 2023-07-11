7829f4db5f378a1cfc7f1800ddc65df221a0482f
nate@lightfeather.io <nate@lightfeather.io>
2023-07-11 15:29:46 -0700

    restyled footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,14 +1,15 @@
 const Footer = () => {
   return (
-    <footer className="bg-black text-md text-white text-center p-3 py-7">
-     
-      copyright © {new Date().getFullYear()} — sam denton. powered by{" "}
-      <a
-        className="underline underline-offset-8"
-        href="mailto:nathanielrbowman@gmail.com"
-      >
-        nathaniel bowman
-      </a>
+    <footer className="bg-black text-md text-white text-center md:text-right p-3 py-7 ">
+      <p>
+        copyright © {new Date().getFullYear()} — sam denton. powered by{" "}
+        <a
+          className="underline underline-offset-8"
+          href="mailto:nathanielrbowman@gmail.com"
+        >
+          nathaniel bowman
+        </a>
+      </p>
     </footer>
   );
 };
