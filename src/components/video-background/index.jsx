da0d6325dbab593be25924a30ab2b7afad092a9c
nate@lightfeather.io <nate@lightfeather.io>
2023-07-31 11:55:26 -0700

    optimized videos

--- src/components/video-background/index.jsx
+++ src/components/video-background/index.jsx
@@ -1,20 +1,24 @@
-import BGVideo from "../../assets/videos/bty-30sec.mp4";
+import BGVideoMP4 from "../../assets/videos/bty-30sec.mp4";
+import BGVideoWEBM from "../../assets/videos/bty-30sec.webm";
 
 const VideoBackground = () => {
   return (
-      <video
-        src={BGVideo}
-        height={"110%"}
-        width={"auto"}
-        id="video"
-        rel="preload"
-        autoPlay
-        loop
-        muted
-        playsInline
-        disablePictureInPicture
-        className="video"
-      />
+    <video
+      height={"110%"}
+      width={"auto"}
+      id="video"
+      rel="preload"
+      autoPlay
+      loop
+      muted
+      poster
+      playsInline
+      disablePictureInPicture
+      className="video"
+    >
+      <source src={BGVideoMP4} type="video/mp4" />
+      <source src={BGVideoWEBM} type="video/webm" />
+    </video>
   );
 };
 
