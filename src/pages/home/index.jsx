f41e432f4bedd816417f219835a536a57b162e51
actuallyitsnathaniel <nathanielrbowman@gmail.com>
2024-05-14 13:30:52 -0700

    comment

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,9 @@ import React from "react";
 // import { MusicLinks } from "../../components/music-embeds";
 import samDentonLogo from "../../assets/images/samdentonstamplogo.png";
 
+// TODO: look how to implement complex grain / particles with three js
+// https://threejs.org/docs/#manual/en/introduction/How-to-use-post-processing
+
 const Home = () => {
   return (
     <div className="h-screen flex justify-center items-center">
